0e80cdd51de18841e61185b95271b9fd1d8cd6ca
tkhskt <luckymortality@gmail.com>
2022-10-29 19:41:40 +0900

    fix: library info

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    `maven-publish`
     kotlin("jvm")
 }
 
@@ -13,4 +14,50 @@ dependencies {
     implementation(libs.bundles.kotlin.poet)
 
     implementation(libs.ksp)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("library") {
+                from(components["java"])
+                group = LibraryInfo.group
+                groupId = LibraryInfo.group
+                version = LibraryInfo.version
+                artifactId = LibraryInfo.artifactId
+
+                pom {
+                    name.set(LibraryInfo.artifactId)
+                    description.set(LibraryInfo.description)
+                    url.set(LibraryInfo.url)
+
+                    scm {
+                        connection.set("scm:git@github.com:tkhskt/glsl-tokenizer-kt.git")
+                        developerConnection.set("scm:git@github.com:tkhskt/glsl-tokenizer-kt.git")
+                        url.set(LibraryInfo.url)
+                    }
+
+                    developers {
+                        developer {
+                            id.set("tkhskt")
+                            name.set("Keita Takahashi")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+project(":processor") {
+    version = LibraryInfo.version
+}
+
+object LibraryInfo {
+    const val artifactId = "shiirudo"
+    const val description = "Sealed Class"
+    const val displayName = "shiirudo"
+    const val group = "com.tkhskt"
+    const val url = "https://github.com/tkhskt/shiirudo"
+    const val version = "1.0.0"
 }
