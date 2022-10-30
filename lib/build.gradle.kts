15a8ba25504c292b2b9cb9201fb1d0e4a3408ec2
tkhskt <luckymortality@gmail.com>
2022-10-30 23:37:05 +0900

    fix: version

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -58,5 +58,5 @@ object LibraryInfo {
     const val displayName = "shiirudo"
     const val group = "com.tkhskt"
     const val url = "https://github.com/tkhskt/shiirudo"
-    const val version = "0.1.0"
+    const val version = "0.1.1"
 }
